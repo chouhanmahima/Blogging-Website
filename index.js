66895b41c85663be395b4bdba6776a49075e342d
Mahima Chouhan <mahimachouhanmahi@gmail.com>
2024-06-08 18:18:58 +0530

    render atlas added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const authRoutes = require("./routes/auth")
 const postRoutes = require("./routes/post");
@@ -7,11 +8,12 @@ const postRoutes = require("./routes/post");
 const authMiddleware = require("./middlewares/auth")
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 
 // Connection with DB(MongoDB)
-mongoose.connect("mongodb://localhost:27017/authapp")
+mongoose.connect(process.env.DB_CONNECTION_URL)
 .then(() => console.log("DB Connected successfully"))
 .catch((err) => console.log(err));
 
@@ -19,7 +21,7 @@ mongoose.connect("mongodb://localhost:27017/authapp")
 app.use("/api/v1/auth",authRoutes);
 app.use("/api/v1/posts", authMiddleware, postRoutes);
 
-const PORT = 8080;
+const PORT = 10000;
 app.listen(PORT, () => {
     console.log(`Server is running at Port : ${PORT}.`);
 })
